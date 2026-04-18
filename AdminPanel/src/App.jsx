403ac572f1bb90aaf6dd0f0be33b1595d29e0caf
Thummala Thivikram <thivikramthummala14@gmail.com>
2026-04-18 16:29:08 +0530

    Updated API URL

--- AdminPanel/src/App.jsx
+++ AdminPanel/src/App.jsx
@@ -8,8 +8,8 @@ import Orders from "./pages/Orders/Orders"
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 const App = () => {
-   const url="http://localhost:4000";
-  //const url="https://fooddeliveryapp-backend-xeb9.onrender.com";
+  //const url="http://localhost:4000";
+  const url = "https://fooddeliveryapp-fnat.onrender.com";
   return (
     
     
